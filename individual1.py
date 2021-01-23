06c3b8f131bbd259f2330ccf20ffa0239cb219a9
E-Sakhno <miss.saxno@yandex.ru>
2021-01-23 10:43:18 +0300

    Добавлен #!/usr/bin/env

--- individual1.py
+++ individual1.py
@@ -1,11 +1,13 @@
-# 17. Дано предложение. Определить число вхождений в него некоторого символа.
-# !/usr/bin/env python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+# 17. Дано предложение. Определить число вхождений в него некоторого символа.
+
 if __name__ == '__main__':
     s = input('Введите предложение: ')
     simb = input('Введите искомый символ: ')
     counter = 0
-    for i in range (0, len(s)):
+    for i in range(0, len(s)):
         if s[i] == simb:
             counter += 1
     print("Число вхождений символа", simb, ' - ', counter)
